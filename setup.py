3266dd0819fac9f8573deb5ff6e50569fdcb5d99
Speedrun <86620608+Bayu12345677@users.noreply.github.com>
2022-06-24 19:49:21 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_package
+import os, sys
+
+Install = ["requests","google"]
+
+setup(
+        author="Bayu Riski A.M",
+        install_requires=Install
+        )
